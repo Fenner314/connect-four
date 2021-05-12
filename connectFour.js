e004a430fafa953106a17bcc24c9fe735194e590
Fenner314 <jacobgfenner@gmail.com>
2021-05-12 07:51:41 -0400

    added hover method to show which piece would be played next

--- connectFour.js
+++ connectFour.js
@@ -4,6 +4,7 @@ class ConnectFour {
         this.cols = 7
         this.selector = selector
         this.createGrid()
+        this.setupEventListeners()
     }
 
     createGrid() {
@@ -11,11 +12,38 @@ class ConnectFour {
         for (let row = 0; row < this.rows; row++) {
             const $row = $('<div>').addClass('row')
             for (let col = 0; col < this.cols; col++) {
-                const $col = $('<div>').addClass('col empty')
+                const $col = $('<div>')
+                    .addClass('col empty')
+                    .attr('data-col', col)
+                    .attr('data-row', row)
                 $row.append($col)
             }
             $board.append($row)
         }
-        console.log($board.html)
+    }
+
+    setupEventListeners() {
+        const $board = $(this.selector)
+
+        function findLastEmptyCell(col) {
+            const cells = $(`.col[data-col='${col}']`)
+            for (let i = cells.length - 1; i >= 0; i--) {
+                const $cell = $(cells[i])
+                if ($cell.hasClass('empty')) {
+                    return $cell
+                }
+            }
+            return null
+        } 
+
+        $board.on('mouseenter', '.col.empty', function() {
+            const col = $(this).data('col')
+            const $lastEmptyCell = findLastEmptyCell(col)
+            $lastEmptyCell.addClass('red-hover')
+        })
+
+        $board.on('mouseleave', '.col', function() {
+            $('.col').removeClass('red-hover')
+        })
     }
 } 
