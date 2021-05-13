59e114c28c82310c83de61f397727ede7e845615
Fenner314 <jacobgfenner@gmail.com>
2021-05-13 10:44:24 -0400

    final product

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     // Make grid
     const connectFour = new ConnectFour('.connect-four')
+
+    connectFour.onPlayerMove = function() {
+        $('#player').text(connectFour.player)
+    }
+
+    $('#restart').click(function() {
+        connectFour.restart()
+    })
 })
--- connectFour.js
+++ connectFour.js
@@ -2,14 +2,19 @@ class ConnectFour {
     constructor(selector) {
         this.rows = 6
         this.cols = 7
-        this.selector = selector
         this.player = 'red'
+        this.selector = selector
+        this.isGameOver = false
+        this.onPlayerMove = function() {}
         this.createGrid()
         this.setupEventListeners()
     }
 
     createGrid() {
         const $board = $(this.selector)
+        $board.empty()
+        this.isGameOver = false
+        this.player = 'red'
 
         for (let row = 0; row < this.rows; row++) {
             const $row = $('<div>').addClass('row')
@@ -40,23 +45,97 @@ class ConnectFour {
         } 
 
         $board.on('mouseenter', '.col.empty', function() {
+            if (that.isGameOver) return
             const col = $(this).data('col')
             const $lastEmptyCell = findLastEmptyCell(col)
             $lastEmptyCell.addClass(`${that.player}-hover`)
         })
 
         $board.on('mouseleave', '.col', function() {
+            if (that.isGameOver) return
             $('.col').removeClass(`${that.player}-hover`)
         })
 
         $board.on('click', '.col.empty', function() {
+            if (that.isGameOver) return
             const col = $(this).data('col')
-            const row = $(this).data('row')
+            // const row = $(this).data('row')
             const $lastEmptyCell = findLastEmptyCell(col)
             $lastEmptyCell.removeClass(`empty ${that.player}-hover`)
             $lastEmptyCell.addClass(`${that.player}-piece`)
+            $lastEmptyCell.data('player', that.player)
+
+            const winner = that.checkForWinner($lastEmptyCell.data('row'), $lastEmptyCell.data('col'))
+            if (winner) {
+                that.isGameOver = true
+                alert(`Game Over! ${that.player} wins!`)
+                $('.col.empty').removeClass('empty')
+                return
+            }
+
             that.player = (that.player === 'red') ? 'black' : 'red'
+            that.onPlayerMove()
             $(this).trigger('mouseenter')
         })
+    }
+
+    checkForWinner(row, col) {
+        const that = this
+
+        function $getCell(i, j) {
+            return $(`.col[data-row='${i}'][data-col='${j}']`)
+        }
+
+        function checkDirection(direction) {
+            let total = 0
+            let i = row + direction.i
+            let j = col + direction.j
+            let $next = $getCell(i, j)
+            while (i >= 0 && 
+                i < that.rows && 
+                j >= 0 && 
+                j < that.cols && 
+                $next.data('player') === that.player
+            )   {
+                total++;
+                i += direction.i
+                j += direction.j
+                $next = $getCell(i, j)
+            }
+            return total
+        }
+
+        function checkWin(directionA, directionB) {
+            const total = 1 + checkDirection(directionA) + checkDirection(directionB)
+            if (total >= 4) {
+                return that.player
+            } else {
+                return null
+            }
+             
+        }
+
+        function checkDiagonalUpRight() {
+            return checkWin({i: 1, j: -1}, {i: 1, j: 1})
+        }
+        
+        function checkDiagonalDownRight() {
+            return checkWin({i: 1, j: 1}, {i: -1, j: -1})
+        }
+        
+        function checkVertical() {
+            return checkWin({i: -1, j: 0}, {i: 1, j: 0})
+        }
+        
+        function checkHorizontal() {
+            return checkWin({i: 0, j: -1}, {i: 0, j: 1})
+        }
+        
+        return checkVertical() || checkHorizontal() || checkDiagonalUpRight() || checkDiagonalDownRight()
+    }
+
+    restart() {
+        this.createGrid()
+        this.onPlayerMove()
     }
 } 
